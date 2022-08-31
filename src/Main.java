efe86306c29252b5f62aed57d5c846bdde8f2804
Alexey Elagin <alexyel@mail.ru>
2022-08-31 13:16:26 +0300

    removeIf in Main aded

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,15 @@ public class Main {
         personsList.add(new Person("Сергей", "Из Трёх Слов", 18));
         personsList.add(new Person("Алексей", "Фамилия Три Слова", 20));
         personsList.add(new Person("Роман", "Фамилия ДваСлова", 20));
+        personsList.add(new Person("Евгений", "Должен Отфильтроваться", 15));
+        personsList.add(new Person("Юрий", "Мал Для СортСписка", 15));
+
 
         System.out.println("Неотсортированный список выглядит так:" + "\n" + personsList);
+
+        personsList.removeIf(person -> person.getAge()<18);
+
+        System.out.println("Исключили из списка если < 18лет:" + "\n" + personsList);
 
         Collections.sort(personsList, new PersonCompare());
         System.out.println("Отсортировванный по условию список выглядит так:" + "\n" + personsList);
